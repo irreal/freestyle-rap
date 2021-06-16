8111a4109e407ab65f86b4f7a1a71bb0e18f9127
Miloš Spasojević <milos.s.pfc@gmail.com>
2021-06-16 10:18:11 +0200

    decrease word list

--- util/createwords.js
+++ util/createwords.js
@@ -1,11 +1,11 @@
 var fs = require("fs");
 
-var content = fs.readFileSync("./wordlist.txt", "utf-8");
+var content = fs.readFileSync("./wordlist2.txt", "utf-8");
 var split = content.split("\n");
 var newContent = split.reduce((tot, cur) => {
   tot += `"${cur}",`;
   return tot;
-}, `{words:[`);
-newContent = newContent.substr(0, newContent.length - 2);
+}, `{"words":[`);
+newContent = newContent.substr(0, newContent.length - 1);
 newContent += `]}`;
 fs.writeFileSync("wordlist.json", newContent, { encoding: "utf-8" });
